39894b570c3856cb6a4ad537de87b25db71a2fcf
congson0710 <congson0710@gmail.com>
2018-05-21 16:14:00 +0700

    update

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
-const Header = () => {
+const Header = (props) => {
     const { textStyle, viewStyle } = styles;
     return (
         <View style={viewStyle}>
-            <Text style={textStyle}>Albums</Text>
+            <Text style={textStyle}>{props.headerText}</Text>
         </View>
     );
 };
@@ -26,6 +26,6 @@ const styles = {
         elevation: 2,
         position: 'relative'
     }
-}
+};
 
 export default Header;
