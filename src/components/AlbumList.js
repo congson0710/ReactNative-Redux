327220313d3425735f6a025ae2f6b2eff3cb1812
congson0710 <congson0710@gmail.com>
2018-05-22 13:34:59 +0700

    refactor with class base component

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
-const AlbumList = () => (
-  <View>
-    <Text>Album list!!!</Text>
-  </View>
-);
+class AlbumList extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>Album list!!!</Text>
+      </View>
+    );
+  }
+}
 
 export default AlbumList;
