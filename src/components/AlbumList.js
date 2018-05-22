0fb0a5fa1242dd8d50f68cc303d4c1d4d27e02a8
congson0710 <congson0710@gmail.com>
2018-05-22 15:20:39 +0700

    install axios and fetch data

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import axios from 'axios';
 
 class AlbumList extends React.Component {
+  componentWillMount() {
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums');
+  }
   render() {
     return (
       <View>
